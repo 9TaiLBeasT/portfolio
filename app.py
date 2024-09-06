9193fb82b9d6afb0c2274290b7f7c92e6c5d1426
ganesh <gtsganesh2005@gmail.com>
2024-09-06 20:24:23 +0530

    custamizing UI

--- app.py
+++ app.py
@@ -1,36 +1,7 @@
 import streamlit as st
 import base64
 
-def img_decode(path):
-    with open(path, "rb") as img_file:
-        return "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
-    
-def social_media_icons(data):
-    social_icons_html = [
-        f"<a href='{data[platform][0]}' target='_blank' style='margin-right: 10px;'>"
-        f"<i class='{data[platform][1]}' style='color: {data[platform][2]};'></i>"
-        f"</a>" for platform in data
-    ]
-    st.write(f"""
-    <div style="display: flex; justify-content: center; margin-bottom: 20px;">
-        {''.join(social_icons_html)}
-    </div>""", unsafe_allow_html=True)
-
-    
-
-def home():
-    
-    # Page configs (tab title, favicon)
-    st.set_page_config(
-        page_title="G Taraka Shiva Ganesh Portfolio",
-        page_icon="☠️",
-        layout="wide",
-        initial_sidebar_state="expanded"
-    )
-    
-    st.sidebar.header("Configuration")
-    
-    
+def background():
     background = f"""
     <style>
     [data-testid="stAppViewContainer"] > .main{{
@@ -50,54 +21,112 @@ def home():
     background-attachment: fixed;
     }}
     
+    [data-testid="stHorizontalBlock"] {{
+    background-image: url("https://devsnap.nyc3.digitaloceanspaces.com/devsnap.me/codepen-VjrZWv.png");
+    background-size: cover;
+    background-position: center; 
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    }}
+    
     </style>
     """
     st.markdown(background, unsafe_allow_html=True)
+    
+    return background
+
+    
+    
+def img_decode(path):
+    with open(path, "rb") as img_file:
+        return "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
+    
+def social_media_icons(data):
+    social_icons_html = [
+        f"<a href='{data[platform][0]}' target='_blank' style='margin-right: 10px;'>"
+        f"<i class='{data[platform][1]}' style='color: {data[platform][2]};'></i>"
+        f"</a>" for platform in data
+    ]
+    st.write(f"""
+    <div class="social-icons-container">
+        {''.join(social_icons_html)}
+    </div>""", unsafe_allow_html=True)
+    
+
+def home():
+    
+        # PDF CV file
+    with open(r"assets/Ganesh_Resume.docx", "rb") as pdf_file:
+        pdf_bytes = pdf_file.read()
+    
+    # Page configs (tab title, favicon)
+    st.set_page_config(
+        page_title="Ganesh's Portfolio",
+        page_icon="☠️",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+    
+    with st.sidebar:
+        st.download_button(
+            label="📄 Download my resume",
+            data=pdf_bytes,
+            file_name="Ganesh_Resume.docx",
+            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            )
+    
+    #background UI
+    background()
 
     # CSS styles file
     with open(r"styles/main.css") as f:
         st.write(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-
-    # PDF CV file
-    with open(r"assets/Ganesh_Resume.docx", "rb") as pdf_file:
-        pdf_bytes = pdf_file.read()
-
-    
-    # Top title
-    st.write(f"""<div class="title"><strong>Hi! My name is</strong> G. Taraka Shiva Ganesh👋</div>""", unsafe_allow_html=True)
-
-    # Profile image
-    st.write(f"""
-    <div class="container">
-        <div class="box">
-            <div class="spin-container">
-                <div class="shape">
-                    <div class="bd">
-                        <img src="{img_decode(r"assets/gun.png")}" alt="Ganesh">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    """, 
-    unsafe_allow_html=True)
-
-    # Subtitle
-    st.write(f"""<div class="subtitle" style="text-align: center;">Machine Learning and Software Engineer</div>""", unsafe_allow_html=True)
-
     st.markdown("""<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">""", unsafe_allow_html=True)
 
-    # Social Icons
     social_icons_data = {
     "Kaggle": ["https://www.kaggle.com/auu23egcse045", "fa-brands fa-kaggle fa-bounce fa-xl", "#20beff"],
     "LinkedIn": ["https://www.linkedin.com/in/gtsganesh/", "fa-brands fa-linkedin fa-beat-fade fa-xl", "#108bea"],
     "GitHub": ["https://github.com/9TaiLBeasT", "fa-brands fa-github fa-beat fa-xl", "#000000"],
     "X": ["https://x.com/GGanesh192349?t=wCMPvfh1d8_aHT4IuTxP_g&s=09", "fa-brands fa-square-x-twitter fa-bounce fa-xl", "#000000"], 
-}
+    }
 
-    
-    social_media_icons(social_icons_data)
+    with st.container(border=False):
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            
+            st.write(f"""<div class="title"><strong>Hi, I'm </strong>Ganesh</div>""", unsafe_allow_html=True)
+            st.write("")
+            st.markdown("""
+                <p class='custom-text'>
+                    I am a <strong>Computer Science Engineering</strong> student deeply fascinated by the world of technology and innovation. With a keen interest in <em>Machine Learning</em> and <em>Artificial Intelligence</em>, I am dedicated to understanding and mastering these transformative technologies. 
+                    My academic journey has equipped me with a strong foundation in programming, data structures, and algorithms, while my passion drives me to explore advanced topics such as neural networks, natural language processing, and computer vision.
+                </p>
+            """, unsafe_allow_html=True)
+
+            st.write("")
+            st.write("")
+            # Social Icons
+            social_media_icons(social_icons_data)
+
+        with col2:
+        # Profile image
+            st.write(f"""
+            <div class="container">
+                <div class="box">
+                    <div class="spin-container">
+                        <div class="shape">
+                            <div class="bd">
+                                <img src="{img_decode(r"assets/gun.png")}" alt="Ganesh">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            """, 
+            unsafe_allow_html=True)
+        
 
     st.write("##")
 
@@ -113,15 +142,6 @@ def home():
     - 🏠 India
     """)
 
-    st.write("##")
-
-    # Download CV button
-    st.download_button(
-        label="📄 Download my resume",
-        data=pdf_bytes,
-        file_name="Ganesh_Resume.docx",
-        mime="application/pdf",
-    )
 
     st.write("##")
     
